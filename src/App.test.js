5f703c31ae4fa042a0a0e963f48e1d5d158c20fe
serghei madan <madan.serghei@yahoo.com>
2023-03-18 00:57:54 +0000

    App test throws an error

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,8 @@
 // Tests in Jest consist of test suites (describe), which are comprised of test cases (it), which have assertions (expect) that turn out green or red:
+import renderer from 'react-test-renderer';
+
+// import App from './App';
+
 
 describe('truthy and falsy', () => {
   // test case
@@ -13,3 +17,22 @@ describe('truthy and falsy', () => {
     expect(false).toBe(false);
   });
 });
+
+// describe('Item', () => {
+//     const item = {
+//         title: "React",
+//         url: 'https://reactjs.org/',
+//         author: 'Jordan Walke',
+//         num_comments: 3,
+//         points: 4,
+//         objectID: 0,
+//     }
+    
+//     it('renders all properties', () => {
+//         const component = renderer.create(<Item item={item} />)
+        
+//         expect(component.root.findByType('a').props.href).toEqual(
+//             'https://reactjs.org/'
+//         );
+//     })
+// })
