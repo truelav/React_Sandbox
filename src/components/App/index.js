8f74bb874ff64e0e2c1a338deb42d67b13b40d6a
serghei madan <madan.serghei@yahoo.com>
2023-03-20 22:46:45 +0000

    FEAT: reconfigured the app for the firebase config

---
+++ src/components/App/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Navigation from "../Navigation";
+import LandingPage from "../Landing";
+import SignUpPage from "../SignUp";
+import SignInPage from "../SignIn";
+import PasswordForgetPage from "../PasswordForget";
+import HomePage from "../Home";
+import AccountPage from "../Account";
+import AdminPage from "../Admin";
+
+import * as ROUTES from "../../constants/routes";
+
+const App = () => (
+  <div>
+    <Navigation />
+
+    <hr />
+    <Routes>
+      <Route exact path={ROUTES.LANDING} element={<LandingPage />} />
+      <Route path={ROUTES.SIGN_UP} element={<SignUpPage />} />
+      <Route path={ROUTES.SIGN_IN} element={<SignInPage />} />
+      <Route path={ROUTES.PASSWORD_FORGET} element={<PasswordForgetPage />} />
+      <Route path={ROUTES.HOME} element={<HomePage />} />
+      <Route path={ROUTES.ACCOUNT} element={<AccountPage />} />
+      <Route path={ROUTES.ADMIN} element={<AdminPage />} />
+    </Routes>
+  </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from "./App";
+import App from "./components/App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
